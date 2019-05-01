6d65a1afaff53ff9c4786cff86f86cd30f7d8fa9
Ehsan Yousefi <mailbox5517@gmail.com>
2019-05-02 00:05:29 +0430

    structural changes

--- Reason.php
+++ Reason.php
@@ -1,60 +1,42 @@
 <?php
 
 /* A Dependency Injection for PHP
- * Author Ehsan Yousefi 
- * copyright 2019 Ehsan Yousefi <mailbox5517@gmail.com> | https://fsdeveloper.ir 
+ * author Ehsan Yousefi 
+ * copyright 2019-2025 Ehsan Yousefi <mailbox5517@gmail.com> | https://fsdeveloper.ir 
  * version 2     */
 class Reason 
 {
+
+    public $Nominees = [];
+
     public function inject($ClassName, $ClassMethod = 0, $data = null)
     { 
-        if(!class_exists($ClassName)) {
-           throw new Exception('Reason'.': class'.var_dump($ClassName).' is not loaded yet.');
+        if (!class_exists($ClassName)) {
+            throw new Exception('Reason'.': class'.var_dump($ClassName).' is not loaded yet.');
         }
-        if($ClassMethod == null){
+        if ($ClassMethod == null) {
             $ReflectionClass = new ReflectionClass($ClassName);
-            
             if($ReflectionClass->getConstructor()){
                 return $ReflectionClass->newInstance($this);
-            }else{
+            } else {
                 return $ReflectionClass->newInstance();
             }
         }
-        
         $ReflectionMethod = new ReflectionMethod($ClassName, $ClassMethod);
         $Parameters = $ReflectionMethod->getParameters();
         $j = count($Parameters);
         $j--;
-        if($j == -1){
-           return $ReflectionMethod->invoke(new $ClassName());
+        if ($j == -1) {
+            return $ReflectionMethod->invoke(new $ClassName());
         }
-        for($i=0; $i<=$j; $i++){
-                $ReflectionParameter = new ReflectionParameter(array($ClassName, $ClassMethod), $i) ;
+        for ($i=0; $i<=$j; $i++) {
+                $id = [$ClassName, $ClassMethod];
+                $ReflectionParameter = new ReflectionParameter($id, $i) ;
                 $MiddleCheck = $ReflectionParameter->name;
-                if(class_exists($MiddleCheck)) {$Parameter[$i] = $MiddleCheck;}
-                else {$j--;}
+                if (class_exists($MiddleCheck)) {
+                    array_push($this->Nominees, new $MiddleCheck);
+                }
         }
-        switch($j){
-        case 0:
-            $ReflectionMethod->invoke(new $ClassName,  new $Parameter[0], $data );  
-            break;
-        case 1:
-            $ReflectionMethod->invoke(new $ClassName, new $Parameter[0],  new $Parameter[1],
-            $data); 
-            break;
-        case 2:
-            $ReflectionMethod->invoke(new $ClassName, new $Parameter[0],  new $Parameter[1], 
-            new $Parameter[2], $data);
-            break;
-        case 3:
-            $ReflectionMethod->invoke(new $ClassName, new $Parameter[0],  new $Parameter[1], 
-            new $Parameter[3],new $Parameter[4], $data);  
-            break;
-        case 4:
-            $ReflectionMethod->invoke(new $ClassName, new $Parameter[0],  new $Parameter[1], 
-            new $Parameter[3],new $Parameter[4], new $Parameter[5], $data);  
-            break;
-        }
+        $ReflectionMethod->invokeArgs(new $ClassName, $this->Nominees);
     }
 }   
-
